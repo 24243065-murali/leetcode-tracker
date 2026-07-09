37c4465d20e015d9a5025648bf780b94db1b6aa2
24243065-murali <24243065@drngpit.ac.in>
2026-07-09 10:06:57 +0530

    Create 7-ReverseInteger

---
+++ 7-ReverseInteger/7-ReverseInteger.java
@@ -0,0 +1,14 @@
+// Last updated: 09/07/2026, 10:06:57
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+
+        if (rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) return 0;
+        return (int) rev;
+    }
+}
